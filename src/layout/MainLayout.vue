0943b07774b1233f2a4a0ee515878385431e3159
Sergey-Kukharenko <Kuharenko@inventive.ru>
2021-11-24 10:19:17 +0300

    master

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <the-navbar />
+  <the-side-bar />
+  <div class="container with-nav">
+    <app-message/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import TheNavbar from "../components/TheNavbar";
+import AppMessage from "../components/ui/AppMessage";
+import TheSideBar from "../components/TheSideBar";
+
+export default {
+  name: "MainLayout",
+  components: {TheSideBar, TheNavbar, AppMessage},
+}
+</script>
+
+<style scoped>
+
+</style>
